e5be24f4921a051766c739526fa5be27e8d95f64
Ben Cohen <benjcohen1985@gmail.com>
2019-07-11 14:25:36 -0400

    Create test environemnt variables

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
+process.env.JWT_SECRET = "test-jwt-secret";
+require("dotenv").config();
 const { expect } = require("chai");
 const supertest = require("supertest");
-
 global.expect = expect;
 global.supertest = supertest;
