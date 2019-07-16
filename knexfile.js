5efdbc824b896408fb57ea54691c436962413514
Ben Cohen <benjcohen1985@gmail.com>
2019-07-16 19:42:09 -0400

    Add knex file before migration

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+const dotenv = require("dotenv").config();
+const config = require("./src/config");
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: process.env.PROD_DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "migrations"
+    }
+  }
+};
