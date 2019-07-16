718f276bb9421859a43942843dd607f655b1a204
Ben Cohen <benjcohen1985@gmail.com>
2019-07-16 14:10:39 -0400

    change env variable for deployment

--- test/users.endpoint.spec.js
+++ test/users.endpoint.spec.js
@@ -2,11 +2,37 @@
 // const bcrypt = require("bcryptjs");
 // const app = require("../src/app");
 // const helpers = require("./test-helpers");
+// const { userServices } = require("../src/Users/userServices");
+// // const testUsers = require("../test/test-helpers");
 
 // describe("Users Endpoints", function() {
-//   const { testUsers } = helpers.makeUsersArray();
 //   let db;
-
+//   const testUsers = [
+//     {
+//       id: 1,
+//       username: "Derek Zoolander",
+//       first_name: "Derek",
+//       last_name: "Zoolander",
+//       password: "$2a$12$FbxELqptGNyLYlXHFvDlK.v7ltcm6pVfQ/jqJuPNNxvRnjlNdejPe",
+//       email: "zoolander@goodlooking.com"
+//     },
+//     {
+//       id: 2,
+//       username: "Boop",
+//       first_name: "Betty",
+//       last_name: "Boop",
+//       password: "$2a$12$JOMHIeJ5U2gnUUb02jk4zeOHuHt4qopiylmaTbRzK0EocdiXdBMj.",
+//       email: "betty@boop"
+//     },
+//     {
+//       id: 3,
+//       username: "Bad Mothaf***a",
+//       first_name: "Jules",
+//       last_name: "Winfield",
+//       password: "$2a$12$3yeFr1aX4CmGWwbYSjkZaeAp1SUFV2SQEkriFbBxmcJR0N50PCQZO",
+//       email: "jules@pulp.com"
+//     }
+//   ];
 //   before("make knex instance", () => {
 //     db = knex({
 //       client: "pg",
@@ -14,157 +40,21 @@
 //     });
 //     app.set("db", db);
 //   });
+//   before(() => {
+//     return db.into("users").insert(testUsers);
+//   });
 //   after("disconnect from db", () => db.destroy());
-//   before("cleanup", () => helpers.cleanTables(db));
-//   afterEach("cleanup", () => helpers.cleanTables(db));
+//   beforeEach(() => db("users").truncate());
 
-//   describe("POST/ api/users", () => {
-//     context("User validation", () => {
-//       beforeEach("insert users", () => helpers.seedUsers(db, testUsers));
-
-//       const requiredFields = [
-//         "username",
-//         "password",
-//         "first_name",
-//         "last_name"
-//       ];
-
-//       requiredFields.forEach(field => {
-//         const registerAttemptBody = {
-//           username: "test username",
-//           first_name: "test first name",
-//           last_name: "test last name",
-//           password: "11AAaa!!"
-//         };
-
-//         it(`responds with 400 required error when ${field} is missing`, () => {
-//           delete registerAttemptBody[field];
-//           return supertest(app)
-//             .post("/api/users")
-//             .send(registerAttemptBody)
-//             .expect(400, {
-//               error: `Missing '${field}' in request body`
-//             });
+//   describe("Get all users", () => {
+//     context(`Given 'users' has data`, () => {
+//       before(() => {
+//         return db.into("users").insert(testArticles);
+//       });
+//       it("return all users", () => {
+//         return userServices.getAllUsers(db).then(actual => {
+//           expect(actual).to.eql(testUsers);
 //         });
-//       });
-//       context("Happy path", () => {
-//         it("responds 201 serialized user, storing bcrypted password", () => {
-//           const newUser = {
-//             username: "test username",
-//             first_name: "test first name",
-//             last_name: "test last name",
-//             password: "11AAaa!!"
-//           };
-//           return supertest(app)
-//             .post("/api/users")
-//             .send(newUser)
-//             .expect(201)
-//             .expect(res => {
-//               expect(res.body).to.have.property("id") +
-//                 expect(res.body.username).to.eql(newUser.username);
-//               expect(res.body.first_name).to.eql(newUser.first_name);
-//               expect(res.body.last_name).to.eql(user.last_name) +
-//                 expect(res.body).to.not.have.property("password");
-//               expect(res.headers.location).to.eql(`/api/users/${res.body.id}`);
-//             })
-//             .expect(res =>
-//               db
-//                 .from("users")
-//                 .select("*")
-//                 .where({ id: res.body.id })
-//                 .first()
-//                 .then(row => {
-//                   expect(row.username).to.eql(newUser.username);
-//                   expect(row.first_name).to.eql(newUser.first_name);
-//                   expect(row.last_name).to.eql(newUser.last_name);
-//                   return bcrypt.compare(newUser.password, row.password);
-//                 })
-//                 .then(compareMatch => {
-//                   expect(compareMatch).to.be.true;
-//                 })
-//             );
-//         });
-//       });
-
-//       it(`responds 400 'password must be longer than 8 characters' when empty password`, () => {
-//         const shortUserPassword = {
-//           username: "test username",
-//           first_name: "test first name",
-//           last_name: "test last name",
-//           password: "1234567"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(userShortPassword)
-//           .expect(400, { error: "Password must be longer than 8 characters" });
-//       });
-
-//       it(`responds 400 'password must be less than 72 characters`, () => {
-//         const userLongPassword = {
-//           first_name: "test first name",
-//           password: "*".repeat(73),
-//           last_name: "test last name",
-//           username: "test username"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(userLongPassword)
-//           .expect(400, {
-//             error: "Password must be less than 72 characters"
-//           });
-//       });
-//       it(`responds 400 error when password start withs spaces`, () => {
-//         const userPasswordStartSpaces = {
-//           username: "test user name",
-//           password: " !sdflksjfl",
-//           first_nam: "test first name",
-//           last_name: "test last name"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(userPasswordStartSpaces)
-//           .expect(400, {
-//             error: "Password must not start with empty spaces"
-//           });
-//       });
-//       it(`responds 400 error when password ends with space`, () => {
-//         const userPasswordEndSpaces = {
-//           username: "test username",
-//           password: "askas ",
-//           first_name: "test first name",
-//           last_name: "test last name"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(userPasswordEndSpaces)
-//           .expect(400, {
-//             error: "Password must not start with empty spaces"
-//           });
-//       });
-//       it(`responds 400 error when password isn't complex enough`, () => {
-//         const userPasswordNotComplex = {
-//           username: "test username",
-//           first_name: "test first name",
-//           last_name: "test last name",
-//           password: "11AAaabb"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(userPasswordNotComplex)
-//           .expect(400, {
-//             error: `password must contain one uppercase, one lowercase, one number and one special character`
-//           });
-//       });
-//       it(`responds 400 'username already taken when username isn't unique`, () => {
-//         const duplicateUser = {
-//           username: users[0].username,
-//           password: "11AAaa!!",
-//           first_name: "test first name",
-//           last_name: "test last name"
-//         };
-//         return supertest(app)
-//           .post("/api/users")
-//           .send(duplicateUser);
 //       });
 //     });
 //   });
