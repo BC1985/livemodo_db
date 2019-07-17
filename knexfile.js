1b70e9364e05dfa98bb2ed6e086decbcf04ae17e
Ben Cohen <benjcohen1985@gmail.com>
2019-07-17 11:00:04 -0400

    Update env

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 module.exports = {
   client: "pg",
-  connection:
-    "postgres://cvlujwrqzurgzu:5434d4d12f33a55ec416278172e07ceb4bd81b1e445143a06d07fb1e7c799289@ec2-174-129-226-232.compute-1.amazonaws.com:5432/dmoqgcft1g9cj",
+  connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
   },
   migrations: {
-    tableName: "migrations"
+    directory: "./migrations"
   },
   ssl: true
 };
