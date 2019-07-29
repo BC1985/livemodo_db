c4459b224a94d6628e25f559599d86ab485f813e
Ben Cohen <benjcohen1985@gmail.com>
2019-07-29 10:31:01 -0400

    Remove comments and unused code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
 const userRouter = require("./Users/userRouter");
 const reviewsRouter = require("./Reviews/ReviewsRouter");
 const authRouter = require("./auth/auth-router");
@@ -20,16 +19,5 @@ app.use("/api/auth", authRouter);
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
-
-// app.use(function errorHandler(error, req, res, next) {
-//   let response;
-//   if (NODE_ENV === "production") {
-//     response = { error: { message: "server error" } };
-//   } else {
-//     console.error(error);
-//     response = { message: error.message, error };
-//   }
-//   res.json(response);
-// });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { PORT, DATABASE_URL, DB_URL } = require("./config");
 const db = knex({
   client: "pg",
   connection: DATABASE_URL
-  // "postgresql://postgres@localhost/livemodo"
+  // For local host replace DATABASE_URL with DB_URL
 });
 
 app.set("db", db);
@@ -13,4 +13,3 @@ app.set("db", db);
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
-//
