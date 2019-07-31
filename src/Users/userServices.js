83f85a1d66f978c88a2d0c25841dea27c1206ace
Ben Cohen <benjcohen1985@gmail.com>
2019-07-31 12:39:23 -0400

    Altered pasword validation message

--- src/Users/userServices.js
+++ src/Users/userServices.js
@@ -34,16 +34,16 @@ const userServices = {
 
   validatePassword(password) {
     if (password.length < 8) {
-      return "password must be at least 8 characters";
+      return "Password must be at least 8 characters";
     }
     if (password.length > 72) {
-      return "password must be less than 72 characters";
+      return "Password must be less than 72 characters";
     }
     if (password.startsWith(" ") || password.endsWith(" ")) {
       return "Password must not start or end with empty spaces";
     }
     if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return "Password must contain one upper case, lower case, number and special character";
+      return "Password must contain one uppercase, lowercase, number and special character";
     }
     return null;
   },
