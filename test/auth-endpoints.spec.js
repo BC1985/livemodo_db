17ad3339192ba5bfe691e5e3a9473364ca0f1db3
Ben Cohen <benjcohen1985@gmail.com>
2019-07-31 17:04:26 -0400

    Add expiry to jwt testing and fix bug

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -128,10 +128,11 @@ describe("Auth Endpoints", function() {
         password: testUser.password
       };
       const expectedToken = jwt.sign(
-        { user_id: testUser.id },
+        { id: testUser.id },
         process.env.JWT_SECRET,
         {
           subject: testUser.username,
+          expiresIn: process.env.JWT_EXPIRY,
           algorithm: "HS256"
         }
       );
