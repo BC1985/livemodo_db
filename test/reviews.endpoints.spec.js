96cefa5856e39757b0461a7b2c6fb79aff88967b
Ben Cohen <benjcohen1985@gmail.com>
2019-08-02 14:18:24 -0400

    Debug test

--- test/reviews.endpoints.spec.js
+++ test/reviews.endpoints.spec.js
@@ -2,53 +2,24 @@ const knex = require("knex");
 const app = require("../src/app");
 const helpers = require("./test-helpers");
 
-function makeReviewsArray() {
-  return [
-    {
-      id: 1,
-      tagline: "tagline",
-      band_name: "band",
-      venue: "venue",
-      show_date: "2017-12-31T05:00:00.000Z",
-      posted: "2017-12-31T05:00:00.000Z",
-      content: "content",
-      rating: 3,
-      username: "username1"
-    },
-    {
-      id: 2,
-      tagline: "tagline",
-      band_name: "band",
-      venue: "venue",
-      show_date: "2017-12-31T05:00:00.000Z",
-      posted: "2017-12-31T05:00:00.000Z",
-      content: "content",
-      rating: 3,
-      username: "username2"
-    }
-  ];
-}
 describe("Reviews Endpoints", function() {
   let db;
-  //   const { testUsers } = helpers.makeUsersArray();
 
-  before("make knex instance", () => {
+  before("make knex instance", done => {
     db = knex({
       client: "pg",
       connection: process.env.TEST_DB_URL
     });
     app.set("db", db);
+    done();
   });
 
   after("disconnect from db", () => db.destroy());
 
-  before("clean the table", () => db.raw("TRUNCATE reviews"));
-
-  afterEach("cleanup", () => db.raw("TRUNCATE reviews"));
-
   describe(`GET /api/reviews`, () => {
     context(`Given no reviews`, () => {
       it(`responds with 200 and an empty list`, () => {
+        //run command 'truncate table reviews;' before running
         return supertest(app)
           .get("/api/reviews")
           .expect(200, []);
@@ -56,16 +27,10 @@ describe("Reviews Endpoints", function() {
     });
 
     context("Given there are reviews in the database", () => {
-      const testUsers = helpers.makeUsersArray();
-      const testReviews = makeReviewsArray();
+      const testReviews = helpers.makeReviewsArray();
 
       beforeEach("insert reviews", () => {
-        return db
-          .into("reviews")
-          .insert(testReviews)
-          .then(() => {
-            return db.into("reviews").insert(testReviews);
-          });
+        return db.into("reviews").insert(testReviews);
       });
 
       it("responds with 200 and all of the reviews", () => {
@@ -87,17 +52,13 @@ describe("Reviews Endpoints", function() {
     });
 
     context("Given there are reviews in the database", () => {
-      const testUsers = helpers.makeUsersArray();
-      const testReviews = makeReviewsArray();
+      const testReviews = helpers.makeReviewsArray();
 
-      beforeEach("insert reviews", () => {
-        return db
-          .into("reviews")
-          .insert(testReviews)
-          .then(() => {
-            return db.into("reviews").insert(testReviews);
-          });
-      });
+      //If no reviews in db, uncomment beforeEach block below
+
+      //   beforeEach("insert reviews", () => {
+      //         return db.into("reviews").insert(testReviews);
+      //   });
 
       it("responds with 200 and the specified Review", () => {
         const ReviewId = 2;
@@ -111,24 +72,19 @@ describe("Reviews Endpoints", function() {
 
   describe(`POST /api/reviews`, () => {
     const testUsers = helpers.makeUsersArray();
-    // beforeEach("insert malicious Review", () => {
-    //   return db.into("users").insert(testUsers);
-    // });
 
     it(`creates a review, responding with 201 and the new review`, () => {
       const newReview = {
-        id: 2,
         tagline: "tagline",
         band_name: "band",
         venue: "venue",
         show_date: "2017-12-31T05:00:00.000Z",
-        posted: "2017-12-31T05:00:00.000Z",
         content: "content",
-        rating: 3,
-        username: "username2"
+        rating: 3
       };
       return supertest(app)
         .post("/api/reviews")
+        .set("Authorization", helpers.makeAuthHeader(testUsers[0]))
         .send(newReview)
         .expect(201)
         .expect(res => {
@@ -183,7 +139,7 @@ describe("Reviews Endpoints", function() {
 
         return supertest(app)
           .post("/api/reviews")
-          .set("Authorization", "bearer", helpers.makeAuthHeader(testUsers[0]))
+          .set("Authorization", helpers.makeAuthHeader(testUsers[0]))
           .send(newReview)
           .expect(400, {
             error: { message: `Missing '${field}' in request body` }
@@ -203,20 +159,15 @@ describe("Reviews Endpoints", function() {
     });
 
     context("Given there are reviews in the database", () => {
-      const testUsers = helpers.makeUsersArray();
-      const testReviews = makeReviewsArray();
+      const testReviews = helpers.makeReviewsArray();
+      // If no reviews in table, uncomment below
 
-      beforeEach("insert reviews", () => {
-        return db
-          .into("reviews")
-          .insert(testReviews)
-          .then(() => {
-            return db.into("reviews").insert(testReviews);
-          });
-      });
+      //   beforeEach("insert reviews", () => {
+      //     return db.into("reviews").insert(testReviews);
+      //   });
 
       it("responds with 204 and removes the review", () => {
-        const idToRemove = 2;
+        const idToRemove = 1;
         const expectedReviews = testReviews.filter(
           Review => Review.id !== idToRemove
         );
@@ -243,20 +194,14 @@ describe("Reviews Endpoints", function() {
     });
 
     context("Given there are reviews in the database", () => {
-      const testUsers = helpers.makeUsersArray();
-      const testReviews = makeReviewsArray();
+      const testReviews = helpers.makeReviewsArray();
 
-      beforeEach("insert reviews", () => {
-        return db
-          .into("reviews")
-          .insert(testReviews)
-          .then(() => {
-            return db.into("reviews").insert(testReviews);
-          });
-      });
+      //   beforeEach("insert reviews", () => {
+      //     return db.into("reviews").insert(testReviews);
+      //   });
 
       it("responds with 204 and updates the Review", () => {
-        const idToUpdate = 2;
+        const idToUpdate = 1;
         const updateReview = {
           tagline: "updated tagline",
           band_name: "band",
@@ -272,7 +217,7 @@ describe("Reviews Endpoints", function() {
           ...updateReview
         };
         return supertest(app)
-          .patch(`/api/reviews/${idToUpdate}`)
+          .put(`/api/reviews/${idToUpdate}`)
           .send(updateReview)
           .expect(204)
           .then(res =>
@@ -285,37 +230,13 @@ describe("Reviews Endpoints", function() {
       it(`responds with 400 when no required fields supplied`, () => {
         const idToUpdate = 2;
         return supertest(app)
-          .patch(`/api/reviews/${idToUpdate}`)
+          .put(`/api/reviews/${idToUpdate}`)
           .send({ irrelevantField: "foo" })
           .expect(400, {
             error: {
               message: `Request body must contain either band name, venue, rating or date`
             }
           });
-      });
-
-      it(`responds with 204 when updating only a subset of fields`, () => {
-        const idToUpdate = 2;
-        const updateReview = {
-          title: "updated Review title"
-        };
-        const expectedReview = {
-          ...testReviews[idToUpdate - 1],
-          ...updateReview
-        };
-
-        return supertest(app)
-          .patch(`/api/reviews/${idToUpdate}`)
-          .send({
-            ...updateReview,
-            fieldToIgnore: "should not be in GET response"
-          })
-          .expect(204)
-          .then(res =>
-            supertest(app)
-              .get(`/api/reviews/${idToUpdate}`)
-              .expect(expectedReview)
-          );
       });
     });
   });
