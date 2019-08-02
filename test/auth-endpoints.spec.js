3e749b5bd27c9ae0a3308a11096c0f54c2d07845
Ben Cohen <benjcohen1985@gmail.com>
2019-08-02 14:15:13 -0400

    Clean up test code

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -2,70 +2,11 @@ const knex = require("knex");
 const jwt = require("jsonwebtoken");
 const app = require("../src/app");
 const helpers = require("./test-helpers");
-const bcrypt = require("bcryptjs");
 
 describe("Auth Endpoints", function() {
   let db;
-  const testUsers = [
-    {
-      id: 1,
-      username: "test-user-1",
-      first_name: "Test user 1",
-      last_name: "test last name 2",
-      password: "password",
-      email: "test@email.com"
-    },
-    {
-      id: 2,
-      username: "test-user-2",
-      first_name: "Test user 2",
-      last_name: "test last name 2",
-      password: "password",
-      email: "test@email.com"
-    },
-    {
-      id: 3,
-      username: "test-user-3",
-      first_name: "Test user 3",
-      last_name: "test last name 3",
-      password: "password",
-      email: "test@email.com"
-    }
-  ];
-  //   const testUsers = [
-  //     {
-  //       id: 1,
-  //       username: "Bob",
-  //       first_name: "Bob",
-  //       last_name: "Smith",
-  //       password: "11AAaa!!",
-  //       email: "test1@email.com"
-  //     },
-  //     {
-  //       id: 2,
-  //       username: "John",
-  //       first_name: "John",
-  //       last_name: "Doe",
-  //       password: "@@22BBbb",
-  //       email: "test2@email.com"
-  //     },
-  //     {
-  //       id: 3,
-  //       username: "Jane",
-  //       first_name: "Jane",
-  //       last_name: "Doe",
-  //       password: "##33CCcc",
-  //       email: "test3@email.com"
-  //     }
-  //   ];
+  const testUsers = helpers.makeUsersArray();
   const testUser = testUsers[0];
-  function seedUsers(db, testUsers) {
-    const preppedUsers = testUsers.map(user => ({
-      ...user,
-      password: bcrypt.hashSync(user.password, 1)
-    }));
-    return db.into("users").insert(preppedUsers);
-  }
   before("make knex instance", () => {
     db = knex({
       client: "pg",
@@ -76,13 +17,7 @@ describe("Auth Endpoints", function() {
 
   after("disconnect from db", () => db.destroy());
 
-  before("cleanup", () => helpers.cleanTables(db));
-
-  afterEach("cleanup", () => helpers.cleanTables(db));
-
   describe(`POST /api/auth/login`, () => {
-    beforeEach("insert users", () => seedUsers(db, testUsers));
-
     const requiredFields = ["username", "password"];
 
     requiredFields.forEach(field => {
@@ -92,6 +27,9 @@ describe("Auth Endpoints", function() {
       };
 
       it(`responds with 400 required error when '${field}' is missing`, () => {
+        beforeEach("insert users", () => {
+          helpers.seedUsers(db, testUsers);
+        });
         delete loginAttemptBody[field];
 
         return supertest(app)
@@ -103,15 +41,18 @@ describe("Auth Endpoints", function() {
       });
     });
 
-    it(`responds 400 'invalid username or password' when bad username`, () => {
-      const userInvalidUser = { username: "user-not", password: "existy" };
+    it(`responds 400 'invalid username' when bad username`, () => {
+      const userInvalidUser = {
+        username: "bad-username",
+        password: testUser.password
+      };
       return supertest(app)
         .post("/api/auth/login")
         .send(userInvalidUser)
         .expect(400, { error: `Incorrect username` });
     });
 
-    it(`responds 400 'invalid username or password' when bad password`, () => {
+    it.only(`responds 400 'invalid password' when bad password`, () => {
       const userInvalidPass = {
         username: testUser.username,
         password: "incorrect"
